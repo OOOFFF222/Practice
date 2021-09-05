dbd08ead6129b732d46a96c2db33a62f6d773e48
Aquila <50446952+chaeeun3@users.noreply.github.com>
2021-09-05 22:45:53 +0900

    Add files via upload

---
+++ java_programming/ex_1/src/ex_1/MainMethod01.java
@@ -0,0 +1,56 @@
+package ex_1;
+import java.util.Date;
+
+public class MainMethod01 {
+	
+	public static void main(String[] args) {
+		/*
+		 * System.out.println("Hello, world."); int intAmount = 10;
+		 * System.out.println(intAmount);
+		 * 
+		 * Date today = new Date();
+		 * 
+		 * boolean isTrue = false;
+		 * 
+		 * char oneChar = '가'; char twoChar = '나';
+		 * 
+		 * byte firstByte = 1; short secondShort = 2; int thirdInt = 3; long forthlong =
+		 * 4;
+		 * 
+		 * int frontData = 2; int backData = 3; int multipleData = frontData + backData;
+		 * System.out.println("multipleData ::: " + multipleData);
+		 * 
+		 * int intA = 0; long longB = 201; long longBintA = intA + longB;
+		 * 
+		 * float floatA = 10.1f; double doubleB = 20.2;
+		 * 
+		 * float floatAB = 10.2f;
+		 * 
+		 * double dfAB = floatA + doubleB; System.out.println("dfAB1 ::: " + dfAB);
+		 */
+		
+		/*
+		 * int accountDocumentCount = 10; int humanResourceDocumentCount = 20; int
+		 * procureDocumentCount = 5; int productionDocumentCount = 15; int
+		 * salesDocumentCount = 25;
+		 * 
+		 * int totalDailyDocumentCount = accountDocumentCount +
+		 * humanResourceDocumentCount + procureDocumentCount + productionDocumentCount +
+		 * salesDocumentCount;
+		 * 
+		 * System.out.println("일일 총 부서 문서 생산량 : " + totalDailyDocumentCount);
+		 * 
+		 * int getFromFrontSalesDocumentCount = 10; int salesDocumentCountFromFront =
+		 * getFromFrontSalesDocumentCount;
+		 */
+		
+		int[] arrA = new int[] {1, 2, 3, 4, 5};
+		int[] arrB = new int[5];
+		arrB[0] = 1;
+		arrB[1] = 2;
+		arrB[2] = 3;
+		arrB[3] = 4;
+		arrB[4] = 5;
+		
+	}
+}
